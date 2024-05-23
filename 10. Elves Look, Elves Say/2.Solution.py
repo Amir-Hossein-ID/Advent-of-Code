f62566e7a86bf2e81efd3309b198b3e1765b8ab4
FTGUPP <52516998+FTGUPP@users.noreply.github.com>
2024-05-23 23:40:40 +0330

    added Day 10 Part 1 and 2

---
+++ 10. Elves Look, Elves Say/2.Solution.py
@@ -0,0 +1,19 @@
+inp = input()
+
+for i in range(50):
+    new = ''
+    last = inp[0]
+    count = 0
+    for j in inp:
+        if j == last:
+            count += 1
+        else:
+            new += str(count) + last
+            count = 1
+            last = j
+
+    if count > 0:
+        new += str(count) + last
+    inp = new
+
+print(len(new))
