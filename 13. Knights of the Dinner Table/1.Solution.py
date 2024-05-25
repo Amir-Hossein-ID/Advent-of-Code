695082c5074a650cc4281a469ce00a3ec155ba07
FTGUPP <52516998+FTGUPP@users.noreply.github.com>
2024-05-25 15:46:51 +0330

    Added Day 13 Part 1 and 2

---
+++ 13. Knights of the Dinner Table/1.Solution.py
@@ -0,0 +1,28 @@
+from itertools import permutations
+
+happ = {}
+
+for i in range(56): # no of lines of input
+    inp = input().strip('.').split()
+    if inp[0] in happ:
+        happ[inp[0]][inp[-1]] = -int(inp[3]) if inp[2] == 'lose' else int(inp[3])
+    else:
+        happ[inp[0]] = {inp[-1]: -int(inp[3]) if inp[2] == 'lose' else int(inp[3])}
+
+new_happ = {}
+
+for i in happ:
+    for j in happ[i]:
+        new_happ[(i, j)] = happ[i][j] + happ[j][i]
+
+total_ans = None
+
+for i in permutations(happ):
+    local_ans = 0
+    for j in range(len(i)-1):
+        local_ans += new_happ[(i[j], i[j+1])]
+    local_ans += new_happ[(i[0], i[-1])]
+    if total_ans == None or local_ans > total_ans:
+        total_ans = local_ans
+
+print(total_ans)
