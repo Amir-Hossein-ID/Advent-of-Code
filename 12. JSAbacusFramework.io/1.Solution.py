7ddfb53fd72572ad58d11725415d1641005a2f4a
FTGUPP <52516998+FTGUPP@users.noreply.github.com>
2024-05-25 11:35:25 +0330

    Added Day 12 Part 1 and 2

---
+++ 12. JSAbacusFramework.io/1.Solution.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+j = json.load(open(os.path.join(os.path.dirname(__file__), 'input.txt')))
+
+def sumJson(js):
+    ans = 0
+    if isinstance(js, list):
+        for i in js:
+            ans += sumJson(i)
+    elif isinstance(js, dict):
+        for i in js:
+            ans += sumJson(js[i])
+    elif isinstance(js, int):
+        return js
+    return ans
+
+print(sumJson(j))
